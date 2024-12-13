280b223756c846207ca12c90d71e2c3a8ce6e8a9
Otavio <aapo5719@gmail.com>
2024-12-13 16:47:50 -0300

    atv11

--- algoritmos/aula10.cpp
+++ algoritmos/aula10.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int num;
+    int num, n1, n2,nota,x;
 
     num = 1;
     // if (num < 4 and num < 7){
@@ -25,17 +25,34 @@ int main(){
     //     cout << "valor nao aceito";
 
     // }
-    if (!num){
-        cout << "VOU AO CLUBE";
+    // if (!num){
+    //     cout << "VOU AO CLUBE";
 
 
 
-    }else {
+    // }else {
 
-        cout << "vou ao cinema ";
+    //     cout << "vou ao cinema ";
 
-    }
+    // }
+//    ;
+    // string res;
+    // cout << "digite a primeira nota" ;
+    // cin >> n1;
+    // cout << "digite a segunda nota" ;
+    // cin >> n2;
+    // nota = n1+n2;
+    // (nota >= 60) ? res= "aprovado" : res = "reprovado";
+    // // cout << "Situação do aluno " << res;
+    // res = (nota >= 60) ? "aprovado" : "reprovado";
+    cout << "digite um valor: ";
+    x = 5;
+    cin >> n1;
 
+    (n1 >= 10) ? x++: x--;
+
+    cout << x;
     return 0;
+
 
 }
