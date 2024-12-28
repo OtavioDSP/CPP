5fa8008ff30d31e8822e44cea96f9a5bd4a09ef0
Otavio <aapo5719@gmail.com>
2024-12-28 16:24:08 -0300

    atv iniciada

--- algoritmos/listasCAVGEmCPP/lista1/ex02.cpp
+++ algoritmos/listasCAVGEmCPP/lista1/ex02.cpp
@@ -2,13 +2,38 @@
 #include <fstream>
 
 using namespace std;
+// int main(){
+//     int i = 0, menor=0, valor=0;
+
+
+//     cout << "insira um numero: \n";
+//     cin >> valor;
+//     menor = valor;
+
+
+//     while(i<9){
+
+//         cout << "insira outro numero: ";
+//         cin >> valor;
+//         cout << "valor: " << valor << "\n";
+
+//         if(valor < menor ){
+
+//             menor = valor;
+
+//         }
+//         cout << "menor: " << menor;
+
+//         i++;
+//     }
+
 int main(){
-    int i = 0, menor=0, valor=0;
+    int i = 0, maior=0, valor=0;
 
 
     cout << "insira um numero: \n";
     cin >> valor;
-    menor = valor;
+    maior = valor;
 
 
     while(i<9){
@@ -17,16 +42,15 @@ int main(){
         cin >> valor;
         cout << "valor: " << valor << "\n";
 
-        if(valor < menor ){
+        if(valor > maior ){
 
-            menor = valor;
+            maior = valor;
 
         }
-        cout << "menor: " << menor;
+        cout << "maior: " << maior;
 
         i++;
     }
-
 
 
 
