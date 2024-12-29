41dc4416d87ff57d94338b12d3f2ccf629893d4a
Otavio <aapo5719@gmail.com>
2024-12-29 18:37:05 -0300

    atv 05 feita

---
+++ algoritmos/listasCAVGEmCPP/lista1/ex05.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    int qtdFunc = 0, i;
+    float salario = 0, mediaSal = 0;
+    cout << "Insira a quantidade de funcionarios: ";
+    cin >> qtdFunc;
+    while(i < qtdFunc){
+        cout << "insira o salario";
+        cin >> salario;
+        mediaSal = mediaSal + salario;
+        i++;
+    }
+    mediaSal = mediaSal / qtdFunc;
+    cout << "media salarial: " << mediaSal;
+
+
+
+}
