aba62d2e3ce4edb7c3abf86ae2901af7ab5f6c81
Otavio <aapo5719@gmail.com>
2024-12-29 18:26:34 -0300

    atv04 feita

---
+++ algoritmos/listasCAVGEmCPP/lista1/ex04.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main (){
+    bool flag = true;
+    int idade = 0, count = 0, i = 0;
+
+    while(flag){
+
+        cout << "Insira a idade: ";
+        cin >> idade;
+
+
+        count = count + idade;
+
+
+        i++;
+
+
+        cout << "idade total: " << count << "\n";
+        cout << "Deseja inserir mais uma idade?\n 0 - nao \n 1 - sim";
+        cin >> idade;
+        if(idade == 0){
+            cout << "media: " << count/i;
+            break;
+        } 
+
+    }
+
+
+}
