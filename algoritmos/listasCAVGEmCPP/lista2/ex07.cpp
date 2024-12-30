6dc9bb187ef27cb252daa9613315e41e5e6da172
Otavio <aapo5719@gmail.com>
2024-12-30 17:40:07 -0300

    atv 07 finalizada

--- algoritmos/listasCAVGEmCPP/lista2/ex07.cpp
+++ algoritmos/listasCAVGEmCPP/lista2/ex07.cpp
@@ -3,21 +3,39 @@
 using namespace std;
 
 int main(){
-    int i=0, num=0, menor = 0;
+    // int i=0, num=0, menor = 0;
+    //     cout << "Insira o numero: ";
+    //     cin >> num;
+    //     menor = num;
+    //     do{
+    //         cout << "Insira o numero: ";
+    //         cin >> num;
+    //         if (num< menor) {
+    //             menor = num;
+    //         }
+
+
+    //         i++;
+    //     }while(i<2);
+    // cout<< menor;
+
+
+
+     int i=0, num=0, maior = 0;
         cout << "Insira o numero: ";
         cin >> num;
-        menor = num;
+        maior = num;
         do{
             cout << "Insira o numero: ";
             cin >> num;
-            if (num< menor) {
-                menor = num;
+            if (num > maior) {
+                maior = num;
             }
 
 
             i++;
         }while(i<2);
-    cout<< menor;
+    cout<< maior;
 
     return 0;
 }
