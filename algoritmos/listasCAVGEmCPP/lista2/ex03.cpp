c453d8686d3fe419c265063e9f52e2c9fd7ca399
Otavio <aapo5719@gmail.com>
2024-12-30 17:05:31 -0300

    atv03 feita

---
+++ algoritmos/listasCAVGEmCPP/lista2/ex03.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int i=0;
+        do{
+            i++;
+            if (i%2 == 0){
+                cout << i << endl;
+            }
+        }while(i<20);
+}
