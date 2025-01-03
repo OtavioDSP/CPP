dfb96179d16a00994b7a331afb111dd2866ae9ac
Otavio <aapo5719@gmail.com>
2025-01-03 15:23:19 -0300

    atv feita

---
+++ algoritmos/listasCAVGEmCPP/lista4/ex04.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+float ent[10], num;
+int i=0;
+
+    for(i=0; i<10; i++){
+        cout << "Insira um numero: ";
+        cin >> num;
+        ent[i] = num/2;
+        cout << ent[i];
+
+    }
+    
+
+
+}
---
+++ algoritmos/listasCAVGEmCPP/lista4/ex05.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int i, vec[500], mp=0;
+    for ( i = 1; i < 500; i++){
+        mp = i * 5;
+        vec[i] = mp;
+        cout << vec[i] << "\n";
+    }
+    
+
+
+
+}
