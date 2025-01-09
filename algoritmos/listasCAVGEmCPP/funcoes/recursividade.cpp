152c8a8436732d7b3b3720fd6c3f8181a75983b5
Otavio <aapo5719@gmail.com>
2025-01-09 19:41:41 -0300

    aprendendo recursividade

---
+++ algoritmos/listasCAVGEmCPP/funcoes/recursividade.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+void contador(int num, int cont = 0);
+
+int main(){
+    int num;
+    cout << "Digite um número: ";
+    cin >> num;
+    contador(num);
+
+    return 0;
+}
+
+// void contador(int num){
+//     for (int i=0; i<num; i++){
+//         cout << i << endl;
+//     }
+
+
+// }
+void contador(int num, int cont){
+    cout << cont << endl;
+    if (cont < num){
+        contador(num,++cont);
+    }
+    
+
+
+}
+
