ec4b3a058721a2272674aca6e467a25edf383432
Otavio <aapo5719@gmail.com>
2025-01-09 13:52:00 -0300

    omissao de args

---
+++ algoritmos/listasCAVGEmCPP/funcoes/OmissaodeArgs.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void imp(string txt ="Ola mundo");
+
+int main(){
+
+    imp();
+
+
+return 0;
+}
+
+void imp(string txt){
+    cout << txt << endl;
+
+
+}
