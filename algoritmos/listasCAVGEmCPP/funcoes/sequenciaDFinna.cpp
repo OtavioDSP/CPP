e46762287dcf39bf943ed7918eb2dcdb8e192bff
Otavio <aapo5719@gmail.com>
2025-01-10 13:57:46 -0300

    finalizada

--- algoritmos/listasCAVGEmCPP/funcoes/sequenciaDFinna.cpp
+++ algoritmos/listasCAVGEmCPP/funcoes/sequenciaDFinna.cpp
@@ -1,41 +1,30 @@
 #include <iostream>
 using namespace std;
-void sequencia(int end, int start = 0);
 
+/*
+res = next + start 
+start = 0 
+next = 1
+//
+s = 0 + n = 1 = 1/ 
 
-int main(){
+*/
+void fibonacci(int end, int start = 0, int next = 1) {
+    // Imprime o valor atual da sequência
+    cout << start << " ";
+    
+    // Condição de parada: a recursão deve continuar até atingir o 'end'
+    if (end > 1) {
+        // A recursão vai continuar com 'next' se tornando o novo 'start' e a soma de 'start' e 'next' se tornando o próximo 'next'
+        fibonacci(end - 1, next, start + next);
+    }
+}
+
+int main() {
     int end;
     cout << "Digite o fim da sequência: ";
     cin >> end;
-    sequencia(end);
+    fibonacci(end);  // Chama a função fibonacci para gerar a sequência
 
-return 0;
+    return 0;
 }
-
-void sequencia(int end, int start){
-    int res = 0, next = 0, inicio = start;
-    if(start < end){
-        
-
-        sequencia(end, ++start);
-
-    }
-
-}
-
-
-/*
-
-start = 0 
-res = 1
-res = 1
-res = 2
-
-
-end = 
-
-
-
-
-
-*/
