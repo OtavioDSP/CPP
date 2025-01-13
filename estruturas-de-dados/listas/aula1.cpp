710826caf04c52c7c628f2f2aebe6683ca99fd7a
Otavio <aapo5719@gmail.com>
2025-01-13 15:34:23 -0300

    listas

--- estruturas-de-dados/listas/aula1.cpp
+++ estruturas-de-dados/listas/aula1.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(){
     list<int> aula;
+    list<int>::iterator it;
     int tam;
     tam = 10;
 
@@ -12,7 +13,14 @@ int main(){
         aula.push_back(i);
     }
 
+    it= aula.begin();
+    advance(it, 7);
+
+    aula.insert(it,0);
+
     cout << "tamanho da listas: "  << aula.size() << endl;
+    
+    aula.reverse();
 
     tam = aula.size();
     for(int i = 0; i < tam; i++){
