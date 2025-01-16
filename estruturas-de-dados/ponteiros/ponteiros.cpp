350ef986da77cbde546fb9f547360aca00f05a78
Otavio <aapo5719@gmail.com>
2025-01-16 16:28:21 -0300

    ponteiros

--- estruturas-de-dados/ponteiros/ponteiros.cpp
+++ estruturas-de-dados/ponteiros/ponteiros.cpp
@@ -3,17 +3,38 @@
 using namespace std;
 
 int main(){
-    string veiculo = "carro";
-    string *pv;
+    // string veiculo = "carro";
+    // string *pv;
 
-    pv = &veiculo;
+    // pv = &veiculo;
 
-    cout << pv << endl;
-    cout << &veiculo<< endl;
+    // cout << pv << endl;
+    // cout << &veiculo<< endl;
 
-    *pv = "moto";
-    cout << *pv << endl;
+    // *pv = "moto";
+    // cout << *pv << endl;
+    int *p, a[10];
+    p = &a[0];
+    // cout << p << endl;
+    *p = 10;
+    cout << a[0] << endl;
+    *(++p);
+    *p =20;
+    cout << a[1] << endl;
+    *(++p);
+    *p =30;
+    cout << a[2] << endl;
+
+
+
+
+
+
+
+
 
 
     return 0;
+
+
 }
