b2cbf197f73f2716593760954f2f51148cb4fdc9
Otavio <aapo5719@gmail.com>
2025-01-16 15:54:11 -0300

    struct modificado

--- estruturas-de-dados/struct/aula.cpp
+++ estruturas-de-dados/struct/aula.cpp
@@ -19,8 +19,8 @@ struct Carro{
         pot = stPot;
         velMax = stVelMax;
         vel = 0;
-
     }
+
     void imprime(){
     cout << name << endl;
     cout << cor << endl;
@@ -44,12 +44,25 @@ struct Carro{
 };
 
 int main() {
-    Carro car1, car2;
-    car1.insere("gol", "cermelho", 2021, 80,160);
-    car2.insere("fusca", "azul", 1980, 60, 50);
-    car1.imprime();
-    car1.mudaVel(200);
-    car1.imprime();
+    Carro *carros = new Carro[5];
+    Carro car1, car2, car3, car4, car5;
+
+    carros[0] =car1;
+    carros[1] =car2;
+    carros[2] =car3;
+    carros[3] =car3;
+    carros[4] =car5;
+
+    carros[0].insere("Gol", "vermelho", 2021, 80, 160);
+    carros[1].insere("fusca","azul", 1978,50,100);
+    carros[2].insere("Celta", "preto",2005, 59,120);
+    carros[3].insere("Uno", "branco", 1999, 50, 110);
+    carros[4].insere("Porsche 930","cinza", 1980, 660, 275);
+
+    for(int i = 0; i< 5; i++){
+        carros[i].imprime();
+
+    }
 
 
     return 0;
