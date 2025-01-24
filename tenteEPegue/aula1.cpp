a1827402eacd9c0c0513703b90cf8df05b24558a
Otavio <aapo5719@gmail.com>
2025-01-24 12:09:15 -0300

    att

--- tenteEPegue/aula1.cpp
+++ tenteEPegue/aula1.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
-#include <vector>
+// #include <vector>
+// #include <stdexcept>
 using namespace std; 
-
+double divide(double, double);
 int main(){
+    // vector <int>num(5);
+    double n1,n2;
+
+    cout << "Digite dois numeros: ";
+    cin >> n1 >> n2;
+    try{ 
+        cout << divide(n1,n2) << endl;
+    }
+    catch(exception& e)
+    {
+        cout << "Erro: " << e.what() << endl;
+    }
     
 
 
 return 0;
 }
+
+double divide(double n1, double n2){
+    return n1/n2;
+};
